31953b0105850d1088fb1fe4e91a07d193993917
John Rooney <366702@ad.seagate.com>
2019-04-11 16:39:24 +0100

    Section 1 finished

--- CA2_NI_POST_AND_CRIME.r
+++ CA2_NI_POST_AND_CRIME.r
@@ -1,5 +1,5 @@
 # Read in the NI post code file and assign to variable NIPostcodes 
-NIPostcodes <-read.csv("NIPostcodes.csv",header = T,na.strings = '',sep=",")
+NIPostcodes <-read.csv("NIPostcodes.csv",header = T,sep=",")
 # using str print the structure
 str(NIPostcodes)
 # Print the first 10 rows of NIPostcodes
@@ -10,44 +10,49 @@ colnames(NIPostcodes) <- c("Organisation Name", "Sub-building Name", "Building N
                            "Town", "County", "Postcode", "x-coordinates",
                            "y-coordinates", "Primary Key") 
 str(NIPostcodes)
-head(NIPostcodes, 3)
 
-#NIPostcodes[is.na(NIPostcodes),]
-# Number of missing values
+# replace all the blank cell vaules with NA
+NIPostcodes[NIPostcodes == ""] <- NA
 
-# Show the total number and mean missing values of the NIPostcode data
-# First replace all missing data with NA
-nipostcodes[NIPostcodes == ""] <- NA
-# Show the total number of missing values for each attribute
-colSums(!is.na(NIPostcodes))
-# Show the mean number of missing values for each attribute
+# count the number of missing values by column
+colSums(is.na(NIPostcodes))
+
+# Calculate the mean number of missing values for each column
 colMeans(is.na(NIPostcodes))
-
-sapply(NIPostcodes,class)
-class(NIPostcodes$County)
-NIPostcodes$County <- as.factor(NIPostcodes$County)
-class(NIPostcodes$County)
 str(NIPostcodes)
+
+# Get the class of each Column
+sapply(NIPostcodes,class)
+
+#Change the class of County to be a categorising factor
+NIPostcodes$County <- as.factor(NIPostcodes$County)
+
+# Check to see what class County is post the change
+class(NIPostcodes$County)
+
+# Print the column names of the dataframe NIPostcodes
 colnames(NIPostcodes)
+# Change the order of the columns, move Primary Key to the first column
 NIPostcodes <- NIPostcodes[,c("Primary Key","Organisation Name", "Sub-building Name", "Building Name",
                               "Number", "Primary Thorfare", "Alt Thorfare",
                               "Secondary Thorfare", "Locality", "Townland",
                               "Town", "County", "Postcode", "x-coordinates",
                               "y-coordinates")]
+
+# Print the column names of the dataframe NIPostcodes
 colnames(NIPostcodes)
-str(NIPostcodes)
-Limavady_data
+
+# Create a new dataframe called Limavady_data that contains records that have 
+# Limavady in the column Locality,Townland and Town
 Limavady_data<-NIPostcodes[grepl("LIMAVADY",NIPostcodes$Locality) & 
                            grepl("LIMAVADY",NIPostcodes$Townland) & 
                            grepl("LIMAVADY",NIPostcodes$Town),]
 
 # Write Limavady data to CSV file
 write.csv(Limavady_data, file = "Limavady_data.csv")
-head(NIPostcodes, 4)
 
+# rename the NIPostcodes dataframe as CleanNIPostcodeData
 CleanNIPostcodeData <- NIPostcodes
 
-str(CleanNIPostcodeData)
 # Write the NIPostcodes data to CSV file called CleanNIPostcodeData
-
 write.csv(NIPostcodes, file = "CleanNIPostcodeData.csv")
